609857fb6987b6de3595a1c6914949eb8dbe307c
aliaksei <alex.circuit.soft@gmail.com>
2024-05-07 13:38:39 +0300

    some fixes

--- library.c
+++ library.c
@@ -6,8 +6,7 @@
 
 int* byteToInt(void* ptr_in, int len) {
     int numInts = len / sizeof(int); // number of ints
-    printf("numInts: %d", numInts);
-    int *arrInts = malloc(numInts * sizeof(int)); // dinamic memory fo int array
+    int *arrInts = malloc(numInts * sizeof(int)); // dynamic memory fo int array
     for (int i = 0; i < numInts; i++) {
         char* bytePtr = (char*)ptr_in + (i * sizeof(int)); // char to ints
         memcpy(&arrInts[i], bytePtr, sizeof(int)); // copy bytes to int array
