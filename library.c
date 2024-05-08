e75b488b6a9f07bbe83ace9dad6e59b344c680a9
aliaksei <alex.circuit.soft@gmail.com>
2024-05-08 17:26:43 +0300

    new lib

--- library.c
+++ library.c
@@ -3,13 +3,22 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <memory.h>
+#include <stdio.h>
+#include <memory.h>
 
-int* byteToInt(void* ptr_in, int len) {
-    int numInts = len / sizeof(int); // number of ints
-    int *arrInts = malloc(numInts * sizeof(int)); // dynamic memory fo int array
-    for (int i = 0; i < numInts; i++) {
-        char* bytePtr = (char*)ptr_in + (i * sizeof(int)); // char to ints
-        memcpy(&arrInts[i], bytePtr, sizeof(int)); // copy bytes to int array
-    };
-    return arrInts;
+struct myFunc(void* ptr_in, int len_in, void* ptr_out, int len_out) {
+struct {
+    int a;
+    char b;
+} str;
+return str;
 }
+//int* byteToInt(void* ptr_in, int len) {
+//    int numInts = len / sizeof(int); // number of ints
+//    int *arrInts = malloc(numInts * sizeof(int)); // dynamic memory fo int array
+//    for (int i = 0; i < numInts; i++) {
+//        char* bytePtr = (char*)ptr_in + (i * sizeof(int)); // char to ints
+//        memcpy(&arrInts[i], bytePtr, sizeof(int)); // copy bytes to int array
+//    };
+//    return arrInts;
+//}
--- library.h
+++ library.h
@@ -3,6 +3,11 @@
 
 #include <glob.h>
 
-int* byteToInt(void* ptr_in, int len);
+//  void* data_in  -- указатель на вх. данные
+//  int len_in     -- записанное кол-во байт вх. данных
+//  void* data_out -- указатель на вых. данные
+//  int len_out    -- максимальное кол-во байт вых. данных
+//  return         -- записанное кол-во байт вых. данных
+int myFunc(void* data_in, int len_in, void* data_out, int len_out);
 
 #endif //VOIDPTR_LIBRARY_H
